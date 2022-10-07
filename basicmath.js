b45a2622b3494b50bf3efeca2aa6722d596afe98
PackJC <34726562+PackJC@users.noreply.github.com>
2022-10-07 01:29:06 -0400

    Add factorial

--- basicmath.js
+++ basicmath.js
@@ -23,8 +23,18 @@ function multiply(a, b) {
     return a * b;
 }
 
+function factorial(a){
+    if (a <= 0) {
+        return 1;
+    }
+    else{
+        return a*factorial(a-1)
+    }
+}
+
 
 module.exports.add = add;
 module.exports.subtract = subtract;
 module.exports.divide = divide;
 module.exports.multiply = multiply;
+module.exports.factorial = factorial;
